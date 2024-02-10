89c34340495dbb50ed3d9276b872a30402de439f
William Wong <wongwil1@students.zhaw.ch>
2024-02-10 11:43:48 +0100

    html done, requires single patch

--- valentines/script.js
+++ valentines/script.js
@@ -1,3 +1,24 @@
+function loadPage() {
+  const box = document.getElementById('box');
+
+  fetch('home.html', {
+    // 👇️ set Accept header to `text/html`
+    headers: {
+      Accept: 'text/html',
+    },
+  })
+    .then(response => response.text())
+    .then(html => {
+      box.innerHTML = html;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+loadPage();
+
+
 function checkName() {
     var inputName = document.getElementById('nameInput').value;
     var errorMessage = document.getElementById('errorMessage');
@@ -12,3 +33,23 @@ function checkName() {
         shootingDiv.innerHTML = '<img src="pics/shooting.gif" alt="Shooting GIF">';
     }
 }
+
+function moveButton() {
+     var button = document.getElementById('noButton');
+
+     // Get window dimensions
+     var windowWidth = window.innerWidth / 2;
+     var windowHeight = window.innerHeight / 2;
+
+     // Calculate random positions
+     var randomX = Math.floor(Math.random() * (windowWidth - button.offsetWidth));
+     var randomY = Math.floor(Math.random() * (windowHeight - button.offsetHeight));
+
+     // Set button position
+     button.style.left = randomX + 'px';
+     button.style.top = randomY + 'px';
+ }
+
+ function yesButton() {
+      window.location.href = 'shesaidyes.html';
+  }
